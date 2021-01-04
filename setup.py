2eda11af904442b4a460a811af75a95c0845297f
7Bpencil <efagot32@gmail.com>
2021-01-04 21:38:05 +0500

    typos, typos, typos are everywhere

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='mygit',
-    version='1.0',
+    version='6.66',
     author='7Bpencil',
     author_email='efagot32@gmail.com',
-    description='small git-clone vcs',
+    description='small git-like vcs',
     entry_points={'console_scripts': ['mygit = mygit.main:start']},
     packages=find_packages()
 )
